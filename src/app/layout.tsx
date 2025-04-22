2b31190b2e9a39a25f108b9a3781a2e0f69fba2d
Yasen Simeonov <y.simeonov@cnsys.bg>
2025-04-22 16:46:33 +0300

    feat: Completed Chapter 9 'Installing HeroUI and React-Icons'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Providers from "@/components/Providers";
 
 export const metadata: Metadata = {
   title: "NextMatch",
@@ -14,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-
+import {Button} from "@heroui/button";
+import {FaRegSmile} from "react-icons/fa";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className='text-3xl text-red-500'>Hello World!</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className='text-3xl text-red-500'>Hello World!</h1>
+            <Button
+                color='primary'
+                variant='bordered'
+                startContent={<FaRegSmile size={20} />}
+            >
+                Test
+            </Button>
+        </div>
+    );
 }
