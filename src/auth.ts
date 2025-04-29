e2883bf079e021ce64d7b0713dfbe0faec30b252
Yasen Simeonov <y.simeonov@cnsys.bg>
2025-04-29 18:28:04 +0300

    feat: Completed Chapter 33 'Using NextAuth callbacks'.

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,15 @@ import authConfig from "./auth.config"
 import { prisma } from "@/lib/prisma";
 
 export const { handlers: { GET, POST }, signIn, signOut, auth } = NextAuth({
+    callbacks: {
+        async session({session, token}) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+
+            return session;
+        }
+    },
     adapter: PrismaAdapter(prisma),
     session: { strategy: 'jwt' },
     ...authConfig,
