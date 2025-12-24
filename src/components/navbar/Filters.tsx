b1c1a1cae581a29a51a4e55dcb0c06d248770043
Yasen Simeonov <y.simeonov@cnsys.bg>
2025-12-24 18:04:31 +0200

    feat: Completed Section 9, Chapter 119 'Adding the Filters component'.

---
+++ src/components/navbar/Filters.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import {FaFemale, FaMale} from "react-icons/fa";
+import {usePathname} from "next/navigation";
+import {Button} from "@heroui/button";
+import {Slider} from "@heroui/slider";
+import {Select, SelectItem} from "@heroui/select";
+
+export default function Filters() {
+    const pathname = usePathname();
+
+    const orderByList = [
+        {label: 'Last active', value: 'updated'},
+        {label: 'Newest members', value: 'created'},
+    ]
+
+    const genders = [
+        {value: 'male', icon: FaMale},
+        {value: 'female', icon: FaFemale},
+    ]
+
+    if (pathname !== '/members') {
+        return null;
+    }
+
+    return (
+        <div className="shadow-md py-2">
+            <div className="flex flex-row justify-around items-center">
+                <div className="text-secondary font-semibold text-xl">Results: 10</div>
+                <div className='flex gap-2 items-center'>
+                    <div>Gender:</div>
+                    {genders.map(({icon: Icon, value}) => (
+                        <Button key={value} size='sm' isIconOnly color='secondary'>
+                            <Icon size={24} />
+                        </Button>
+                    ))}
+                </div>
+                <div className="flex flex-row items-center gap-2 w-1/4">
+                    <Slider
+                        label='Age range'
+                        color='secondary'
+                        size='sm'
+                        minValue={18}
+                        maxValue={100}
+                        defaultValue={[18, 100]}
+                    />
+                </div>
+                <div className="w-1/4">
+                    <Select
+                        size='sm'
+                        fullWidth
+                        placeholder='Order by'
+                        variant='bordered'
+                        color='secondary'
+                        aria-label='Order by selector'
+                    >
+                        {orderByList.map(item => (
+                            <SelectItem key={item.value}>
+                                {item.label}
+                            </SelectItem>
+                        ))}
+                    </Select>
+                </div>
+            </div>
+        </div>
+    )
+}
